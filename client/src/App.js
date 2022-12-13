4fdfd5857e2599719af7bb39ea55adb087857d4a
Christopher Arcos <arcoschris456@gmail.com>
2022-12-13 14:48:26 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainNav from './components/MainNav';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <MainNav />
+      <div className='card-container'>
+        {/* loop over card component */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
